0436d3cd4c4f5d388fc8de2d23795633f3839ac1
Sambulo Senda <admin@users-MacBook-Air.local>
2020-08-17 16:45:31 +0100

    player added

--- src/components/card/styles/card.js
+++ src/components/card/styles/card.js
@@ -75,9 +75,9 @@ export const Meta = styled.div`
 export const Image = styled.img`
   border: 0;
   width: 100%;
-  max-width: 305px;
+
   cursor: pointer;
-  height: auto;
+  height: 170ppx;
   padding: 0;
   margin: 0;
 `;
--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,15 +1,17 @@
 import React, { useState, useEffect, useContext } from 'react';
-import { Header } from '../components';
+import Fuse from 'fuse.js';
+import { Card, Loading, Header, Player } from '../components';
 import * as ROUTES from '../constants/routes';
 import { FirebaseContext } from '../context/firebase';
 import { SelectProfileContainer } from './profiles';
 import { FooterContainer } from './footer';
 
-export function BrowseContainer() {
+export function BrowseContainer({ slides }) {
     const [category, setCategory] = useState('series');
     const [profile, setProfile] = useState({});
     const [loading, setLoading] = useState(true);
     const [searchTerm, setSearchTerm] = useState('');
+    const [slideRows, setSlideRows] = useState([]);
     
     const { firebase } = useContext(FirebaseContext);
     
@@ -24,9 +26,24 @@ export function BrowseContainer() {
         }, 3000);
     }, [user])
     
+    useEffect(() => {
+        setSlideRows(slides[category]);
+    }, [slides, category]);
+    
+    useEffect(() => {
+      const fuse = new Fuse(slideRows, { keys: ['data.description', 'data.title', 'data.genre'] });
+      const results = fuse.search(searchTerm).map(({ item }) => item);
+      
+      if (slideRows.length > 0 && searchTerm.length > 3 && results.length > 0) {
+          setSlideRows(results);
+      } else {
+          setSlideRows(slides[category]);
+      }
+    }, [searchTerm])
+    
     return profile.displayName ? (
         <>
-      
+        {loading ? <Loading src={user.photoURL} /> : <Loading.ReleaseBody />}
         
             <Header src="joker1" dontShowOnSmallViewPort>
                 <Header.Frame>
@@ -71,6 +88,31 @@ export function BrowseContainer() {
                     <Header.PlayButton>Play</Header.PlayButton>
                 </Header.Feature>
             </Header>
+            
+            <Card.Group>
+                {slideRows.map((slideItem) => (
+                    <Card key={`${category}-${slideItem.title.toLowerCase()}`}>
+                        <Card.Title>{slideItem.title}</Card.Title>
+                        <Card.Entities>
+                            {slideItem.data.map((item) => (
+                                <Card.Item key={item.docId} item={item}>
+                                    <Card.Image src={`/images/${category}/${item.genre}/${item.slug}/small.jpg`} />
+                                    <Card.Meta>
+                                        <Card.SubTitle>{item.title}</Card.SubTitle>
+                                        <Card.Text>{item.description}</Card.Text>
+                                    </Card.Meta>
+                                </Card.Item>
+                            ))}
+                        </Card.Entities>
+                        <Card.Feature category={category}>
+                            <Player>
+                                <Player.Button />
+                                <Player.Video />
+                            </Player>
+                        </Card.Feature>
+                    </Card>
+                ))}
+            </Card.Group>
             <FooterContainer />
         </>)
         : (<SelectProfileContainer user={user} setProfile={setProfile} />);
